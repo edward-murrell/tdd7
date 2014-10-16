0037cfe54b4204c64291ab819a735485b86694db
Edward Murrell <edward@catalyst-au.net>
2014-10-16 11:04:31 +1100

    DCO-437: Add stub file for OUAMockDatabaseTestCase

---
+++ mocks/tests/MockDatabaseTest.php
@@ -0,0 +1,9 @@
+<?php
+/** 
+ * @file Test Drupal Database DatabaseConnection_unittest mock
+ */
+
+namespace oua\lms\testframework\mocks;
+
+class OUAMockDatabaseTestCase extends \PHPUnit_Framework_TestCase {
+}
