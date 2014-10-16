093f8d94c00ebe3132d01aaf7ca4a7d5dabde7e2
Edward Murrell <edward@catalyst-au.net>
2014-10-16 11:20:36 +1100

    DCO-437: Create first unit test of DB Mock.

--- mocks/tests/MockDatabaseTest.php
+++ mocks/tests/MockDatabaseTest.php
@@ -5,10 +5,18 @@
 
 namespace oua\lms\testframework\mocks;
 
+define ('TABLE1', 'testMockTable1');
+define ('TABLE2', 'testMockTable2');
+
 class OUAMockDatabaseTestCase extends \PHPUnit_Framework_TestCase {
   private $db;
 
   public function setUp() {
     $this->db = DatabaseConnection_unittest::getInstance();
+  }
+
+  public function testAddData() {
+    $this->db->addTestData('TABLE1', array());
+    $this->assertCount(1, $this->db->getTestData());
   }
 }
