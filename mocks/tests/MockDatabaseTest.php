78a748f118b4c40350d4fe83446276d3ed1b0ef9
Edward Murrell <edward@catalyst-au.net>
2014-10-16 11:18:46 +1100

    DCO-437: Create mock db connection for testing.

--- mocks/tests/MockDatabaseTest.php
+++ mocks/tests/MockDatabaseTest.php
@@ -6,4 +6,9 @@
 namespace oua\lms\testframework\mocks;
 
 class OUAMockDatabaseTestCase extends \PHPUnit_Framework_TestCase {
+  private $db;
+
+  public function setUp() {
+    $this->db = DatabaseConnection_unittest::getInstance();
+  }
 }
