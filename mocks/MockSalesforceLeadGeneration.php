0eebaae87fc71496a71e7a3f2ac9c8bdcb905779
Edward Murrell <edward@catalyst-au.net>
2014-10-27 12:54:41 +1100

    DCO-504: Fill out MockSalesforceLeadGenerationService with methods.

--- mocks/MockSalesforceLeadGeneration.php
+++ mocks/MockSalesforceLeadGeneration.php
@@ -14,4 +14,26 @@ namespace oua\lms\testframework\mocks;
  */
 class MockSalesforceLeadGenerationService {
 
+  /**
+   * @param string $endpoint URL of salesforce endpoint
+   */
+  public function __construct($endpoint = '') {}
+
+  /**
+   * @param array $data Array of data to send to salesforce. Keys will overwrite
+   *  New keys will be added, existing keys will be overwritten.
+   */
+  public function setData(array $data = array()) {}
+
+  /**
+   * Get the JSON string that will be sent to salesforce.
+   * @return string JSON string of data with all data.
+   */
+  public function getJSON() {}
+
+  /**
+   * Send the data to salesforce
+   * @return TRUE if operation was sucessful, FALSE if there was a failure.
+   */
+  public function send() {}
 }
