2d728ea6a5aa82adf80ae2663e5b701925b22d5b
Edward Murrell <edward@catalyst-au.net>
2014-10-27 12:48:28 +1100

    DCO-504: Stub of Mock salesforce lead generation.

---
+++ mocks/MockSalesforceLeadGeneration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Mock for lead generation submission to salesforce.
+ * @author Edward Murrell <edward@catalyst-au.net>
+ */
+
+namespace oua\lms\testframework\mocks;
+
+/**
+ * Class MockSalesforceLeadGenerationService
+ *
+ * @package oua\lms\services
+ */
+class MockSalesforceLeadGenerationService {
+
+}
