ab4729d80df97f6d69b14d6a6e757a8bc733064c
Edward Murrell <edward@catalyst-au.net>
2014-10-28 15:39:56 +1100

    DCO-504: Move fake variable_set/get in mocks into namespace & class

--- mocks/MockDrupalFunctions.php
+++ mocks/MockDrupalFunctions.php
@@ -3,26 +3,29 @@
  * @file
  * Mocked up Drupal core functions
  * @author Edward Murrell <edward@catalyst-au.net>
+ * @todo: Make this part of DrupalApiService
  */
 
-namespace oua\lms\testframework\mocks;
+namespace oua\lms\testframework\mocks {
+  class DrupalFunctions {
+    /**
+     * Mock version of variable_set()
+     * Original documentation: https://api.drupal.org/api/drupal/includes!bootstrap.inc/function/variable_set/7
+     * @param string $name: The name of the variable to set.
+     * @param string $value: The value to set. This can be any PHP data type; these
+     *  functions take care of serialization as necessary.
+     */
+    function variable_set($name, $value) {
+    }
 
-/**
- * Mock version of variable_set()
- * Original documentation: https://api.drupal.org/api/drupal/includes!bootstrap.inc/function/variable_set/7
- * @param string $name: The name of the variable to set.
- * @param string $value: The value to set. This can be any PHP data type; these
- *  functions take care of serialization as necessary.
- */
-function variable_set($name, $value) {
-}
-
-/**
- * Mock version of variable_set()
- * Original documentation: https://api.drupal.org/api/drupal/includes!bootstrap.inc/function/variable_get/7
- * @param string $name: The name of the variable to return.
- * @param string $default: The default value to use if this variable has never
- *  been set.
- */
-function variable_get($name, $default = NULL) {
+    /**
+     * Mock version of variable_set()
+     * Original documentation: https://api.drupal.org/api/drupal/includes!bootstrap.inc/function/variable_get/7
+     * @param string $name: The name of the variable to return.
+     * @param string $default: The default value to use if this variable has never
+     *  been set.
+     */
+    function variable_get($name, $default = NULL) {
+    }
+  }
 }
