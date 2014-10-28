f6c14981c17045ff724972f1e699e290768a93c1
Edward Murrell <edward@catalyst-au.net>
2014-10-28 15:51:24 +1100

    DCO-504: Update fake variable_get to fall back to real values

--- mocks/MockDrupalFunctions.php
+++ mocks/MockDrupalFunctions.php
@@ -21,17 +21,21 @@ namespace oua\lms\testframework\mocks {
     }
 
     /**
-     * Mock version of variable_set()
+     * Mock version of variable_get().
+     * Returns the mock variables, if set. If a fake variable is not set, then
+     *  the function will attempt to return a version from Drupal. If this does
+     *  not exist, then the $default will be returned.
      * Original documentation: https://api.drupal.org/api/drupal/includes!bootstrap.inc/function/variable_get/7
      * @param string $name: The name of the variable to return.
-     * @param string $default: The default value to use if this variable has never
-     *  been set.
+     * @param string $default: The default value to use if this variable has
+     *  never been set.
+     * @return fake variable, real variable, or default
      */
     public static function variable_get($name, $default = NULL) {
       if (array_key_exists($name, self::$variables)) {
         return self::$variables[$name];
       } else {
-        return $default;
+        return \variable_get($name, $default);
       }
     }
   }
