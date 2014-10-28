716679c950e0e5e48fe55997a9407e8277275c83
Edward Murrell <edward@catalyst-au.net>
2014-10-28 15:41:29 +1100

    DCO-504: Make static variable and functions for variable_get/set

--- mocks/MockDrupalFunctions.php
+++ mocks/MockDrupalFunctions.php
@@ -8,6 +8,7 @@
 
 namespace oua\lms\testframework\mocks {
   class DrupalFunctions {
+    private static $variables = array();
     /**
      * Mock version of variable_set()
      * Original documentation: https://api.drupal.org/api/drupal/includes!bootstrap.inc/function/variable_set/7
@@ -15,7 +16,7 @@ namespace oua\lms\testframework\mocks {
      * @param string $value: The value to set. This can be any PHP data type; these
      *  functions take care of serialization as necessary.
      */
-    function variable_set($name, $value) {
+    public static function variable_set($name, $value) {
     }
 
     /**
@@ -25,7 +26,7 @@ namespace oua\lms\testframework\mocks {
      * @param string $default: The default value to use if this variable has never
      *  been set.
      */
-    function variable_get($name, $default = NULL) {
+    public static function variable_get($name, $default = NULL) {
     }
   }
 }
