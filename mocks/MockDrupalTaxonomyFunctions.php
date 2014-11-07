0e7f5a435225da86fcaa73e939ebb4c6aa374a84
Edward Murrell <edward@catalyst-au.net>
2014-11-07 15:10:40 +1100

    DCO-537: Add documentation to stub mock methods.

--- mocks/MockDrupalTaxonomyFunctions.php
+++ mocks/MockDrupalTaxonomyFunctions.php
@@ -19,7 +19,28 @@ namespace oua\lms\testframework\mocks {
       return $nid_map[$tid];
     }
 
+    /**
+     * Return a hierarchical representation of a vocabulary.
+     * @param int $vid Which mock vocabulary to generate the tree for.
+     * @param int $parent The term ID under which to generate the tree.
+     *  If 0, generate the tree for the entire vocabulary.
+     * @param int $max_depth The number of levels of the tree to return. Leave
+     *  NULL to return all levels.
+     * @param boolean $load_entities If TRUE, a full entity load will occur on
+     *  the term objects. Otherwise they are partial objects. Defaults to FALSE.
+     * @return An array of all mock term objects in the tree. Each term object
+     *  is extended to have "depth" and "parents" attributes in addition to its
+     *  normal ones. Term objects will be partial or complete depending on the
+     *  $load_entities parameter.
+     */
     public static function taxonomy_get_tree($vid, $parent = 0, $max_depth = NULL, $load_entities = FALSE) {}
-    public static function taxonomy_term_load($tid) {}
+
+    /**
+     * Return the mock term object matching a term ID.
+     *  Original documentation: https://api.drupal.org/api/drupal/modules!taxonomy!taxonomy.module/function/taxonomy_term_load/7
+     * @param int $tid Taxonomy ID of term to load.
+     * @return A taxonomy term object, or FALSE if the term was not found.
+     */
+    public static function taxonomy_term_load($tid = null) {}
   }
 }
