774b389f72be77db3b85832b5e45a25210d00225
David Walters <david.walters@open.edu.au>
2014-11-14 06:40:26 +1100

    LT-721: added safeJson() to BasicTestCase so we can live with the php53 nightmare

--- basefixtures/BasicTestCase.php
+++ basefixtures/BasicTestCase.php
@@ -11,7 +11,26 @@ require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_override_server_variables();
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
+/**
+ * Class BasicTestCase
+ * @package oua\lms\testframework
+ */
 class BasicTestCase extends \PHPUnit_Framework_TestCase {
 
+  /**
+   * Wrapper to handle the php53 nightmare.
+   *
+   * @param object|array $obj
+   *   The object to jsonify.
+   *
+   * @return string
+   *   The json string result.
+   */
+  protected static function safeJson($obj) {
+    if (defined("JSON_PRETTY_PRINT")) {
+      return json_encode($obj, JSON_PRETTY_PRINT);
+    }
+    return json_encode($obj);
+  }
 
 }
