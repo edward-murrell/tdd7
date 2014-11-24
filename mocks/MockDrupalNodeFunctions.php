2913bd65bea7dcf2ff09ba7fa9aa8a546651cae2
Edward Murrell <edward@catalyst-au.net>
2014-11-25 10:39:38 +1100

    DCO-549: Fix first unit test failure by creating AddMockNode()

--- mocks/MockDrupalNodeFunctions.php
+++ mocks/MockDrupalNodeFunctions.php
@@ -8,6 +8,30 @@
 namespace oua\lms\testframework\mocks {
   class MockDrupalNodeFunctions {
     /**
+     * List of nodes, indexed by Node ID.
+     * @var array
+     */
+    private static $nodes = array();
+
+    /**
+     * Add basic node to fake node list with field as parameters.
+     *
+     * @param int $nid
+     *   Node ID of this node.
+     * @param string $type
+     *   Node type of this node. eg; "page".
+     * @param string $title
+     *   Optional title of node.
+     */
+    public static function AddMockNode($nid, $type, $title = '') {
+      $node = new \stdClass;
+      $node->nid = $nid;
+      $node->type = $type;
+      $node->title = $title;
+      self::$nodes[$nid] = $node;
+    }
+
+    /**
      * Delete all saved mock data.
      */
     public static function ResetMockData() {
