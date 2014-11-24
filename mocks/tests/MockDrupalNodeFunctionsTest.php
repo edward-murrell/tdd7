29f5001e06d7d279b4b1e66110c6c3fb8d980b8d
Edward Murrell <edward@catalyst-au.net>
2014-11-25 09:37:28 +1100

    DCO-549: Create failing unit tests to enforce addition nodes to Mock Node Functions.

--- mocks/tests/MockDrupalNodeFunctionsTest.php
+++ mocks/tests/MockDrupalNodeFunctionsTest.php
@@ -12,8 +12,35 @@ require_once DRUPAL_ROOT . '/sites/all/modules/custom/oua_lms_testframework/mock
 
 use oua\lms\testframework\BasicTestCase;
 
+define ('MOCK_NODE_TEST_NID1', 547545754543);
+define ('MOCK_NODE_TEST_NID2', 963732177731);
+define ('MOCK_NODE_TEST_NID_TYPE1', 'test_type_one');
+define ('MOCK_NODE_TEST_NID_TYPE1', 'test_type_two');
+define ('MOCK_NODE_TEST_NID_TITLE1', 'Testing Title Uno');
+define ('MOCK_NODE_TEST_NID_TITLE2', 'Testing Title Duo');
+
 class MockDrupalTaxonomyFunctionsTest extends \oua\lms\testframework\BasicTestCase {
   public function testResetMockDataFunctionExists() {
     MockDrupalNodeFunctions::ResetMockData();
+  }
+
+  /**
+   * GIVEN AddMockNode() is called.
+   * AND NID is provided
+   * THEN a stdClass is returned
+   * WITH nid set, type set.
+   */
+  public function testNode_loadReturnsClassWithType() {
+    MockDrupalNodeFunctions::AddMockNode(MOCK_NODE_TEST_NID1, MOCK_NODE_TEST_NID_TYPE1, MOCK_NODE_TEST_NID_TITLE1);
+    MockDrupalNodeFunctions::AddMockNode(MOCK_NODE_TEST_NID2, MOCK_NODE_TEST_NID_TYPE2, MOCK_NODE_TEST_NID_TITLE2);
+    $node2 = MockDrupalNodeFunctions::node_load(MOCK_NODE_TEST_NID2);
+    $node1 = MockDrupalNodeFunctions::node_load(MOCK_NODE_TEST_NID1);
+    $this->assertEquals(547545754543,        $node1->nid);
+    $this->assertEquals('test_type_one',     $node1->type);
+    $this->assertEquals('Testing Title Uno', $node1->title);
+
+    $this->assertEquals(963732177731,        $node2->nid);
+    $this->assertEquals('test_type_two',     $node2->type);
+    $this->assertEquals('Testing Title Duo', $node2->title);
   }
 }
