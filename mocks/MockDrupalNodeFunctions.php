b45f02a989d748ff254ebbea9f8a70e6aeb85647
Edward Murrell <edward@catalyst-au.net>
2014-11-26 13:58:01 +1100

    DCO-549: Update MockDrupalNodeFunctions::node_load comments in line with coding standards.

--- mocks/MockDrupalNodeFunctions.php
+++ mocks/MockDrupalNodeFunctions.php
@@ -17,13 +17,14 @@ namespace oua\lms\testframework\mocks {
      * Return a mock node object.
      *
      * @param int $nid
-     *  The node ID to load.
+     *   The node ID to load.
      * @param int $vid
-     *  The revision ID. NOT YET IMPLMENTED IN MOCK.
-     * @param boolean $reset
-     *  Whether to reset the node_load_multiple cache. IGNORED IN MOCK.
+     *   The revision ID. NOT YET IMPLMENTED IN MOCK.
+     * @param bool $reset
+     *   Whether to reset the node_load_multiple cache. IGNORED IN MOCK.
      *
-     * @return A fully-populated node object, or FALSE if the node is not found.
+     * @return mixed
+     *   A fully-populated node object, or FALSE if the node is not found.
      */
     public static function node_load($nid = NULL, $vid = NULL, $reset = FALSE) {
       if (array_key_exists($nid, self::$nodes)) {
