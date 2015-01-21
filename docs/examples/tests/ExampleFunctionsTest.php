8d83c17d676ce5d0e7f034183c0cf0ebdf86ba89
Edward Murrell <edward@catalyst-au.net>
2015-01-21 03:53:38 +0000

    Create example testGet_node_titleReturnsExpectedString with seutp/tear down using framework.

--- docs/examples/tests/ExampleFunctionsTest.php
+++ docs/examples/tests/ExampleFunctionsTest.php
@@ -12,14 +12,29 @@ require_once __DIR__ . '/../../../basefixtures/BasicTestCase.php';
 require_once __DIR__ . '/../../../mocks/MockDrupalNodeFunctions.php';
 
 use oua\lms\testframework\BasicTestCase;
+use \oua\lms\testframework\mocks\MockDrupalNodeFunctions;
+
+/**
+ * Use the mock node_load() function, instead of the core drupal node_load().
+ */
+function node_load($nid = NULL, $vid = NULL, $reset = FALSE) {
+  return MockDrupalNodeFunctions::node_load($nid, $vid, $reset);
+}
 
 class ExampleFunctionsTest extends \oua\lms\testframework\BasicTestCase {
 
   public function setUp() {
-    
+    MockDrupalNodeFunctions::AddMockNode(TDD7_EXAMPLE_NID1, 'page', 'Expected title');
   }
 
   public function tearDown() {
-    
+    MockDrupalNodeFunctions::ResetMockData();
+  }
+
+  /**
+   * Test the get_node_title returns a title for the right node.
+   */
+  public function testGet_node_titleReturnsExpectedString() {
+    $this->assertEquals('Expected title', get_node_title());
   }
 }
