7b538a48ff450c573a64bf23934ecb43880e7352
Edward Murrell <edward@catalyst-au.net>
2015-01-21 03:27:55 +0000

    Create first Example Functions & Example Functions Test for doc purposes.

---
+++ docs/examples/tests/ExampleFunctionsTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * @author Edward Murrell <edward@catalyst-au.net>
+ * Test get_node_title
+ */
+
+namespace tdd7\example;
+
+require_once __DIR__ . '/../ExampleFunctions.inc';
+require_once __DIR__ . '/../../../basefixtures/BasicTestCase.php';
+require_once __DIR__ . '/../../../mocks/MockDrupalNodeFunctions.php';
+
+use oua\lms\testframework\BasicTestCase;
+
+class ExampleFunctionsTest extends \oua\lms\testframework\BasicTestCase {
+
+  public function setUp() {
+    
+  }
+
+  public function tearDown() {
+    
+  }
+}
