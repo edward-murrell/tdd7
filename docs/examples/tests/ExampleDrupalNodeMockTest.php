c65f9a9350902f5fcbd1c1c31d40e9d07c812331
Edward Murrell <edward@catalyst-au.net>
2015-01-27 02:32:30 +0000

    Add more explanation code to ExampleDrupalNodeMockTest.

--- docs/examples/tests/ExampleDrupalNodeMockTest.php
+++ docs/examples/tests/ExampleDrupalNodeMockTest.php
@@ -18,7 +18,8 @@ use oua\lms\testframework\BasicTestCase;
 use \oua\lms\testframework\mocks\MockDrupalNodeFunctions;
 
 /**
- * Use the mock node_load() function, instead of the core drupal node_load().
+ * Define the mock node_load() function for our namespaced production code,
+ * instead of the core drupal node_load().
  */
 function node_load($nid = NULL, $vid = NULL, $reset = FALSE) {
   return MockDrupalNodeFunctions::node_load($nid, $vid, $reset);
@@ -26,22 +27,35 @@ function node_load($nid = NULL, $vid = NULL, $reset = FALSE) {
 
 class ExampleDrupalNodeMockTest extends BasicTestCase {
 
+  /**
+   * This code is run before each testABC() method in this class.
+   */
   public function setUp() {
-    // Create a mock node with nid defined in TDD7_EXAMPLE_NID1, type 'page', and a title of 'Expected title'
+    /**
+     * Create a mock node with nid defined in TDD7_EXAMPLE_NID1, type 'page', and a title of 'Expected title'
+     */
     MockDrupalNodeFunctions::AddMockNode(TDD7_EXAMPLE_NID1, 'page', 'Expected title');
   }
 
+  /**
+   * This code is run after each testABC() method in this class.
+   */
   public function tearDown() {
-    // Delete all mock node data.
+    /**
+     * Delete all mock node data, so that test data from one class does not
+     * potentially corrupt the next test.
+     */
     MockDrupalNodeFunctions::ResetMockData();
   }
 
   /**
-   * Test the get_node_title returns a title for the right node.
+   * Test the get_node_title() returns a title for the right node.
    */
   public function testGet_node_titleReturnsExpectedString() {
-    // Retrieve the results of get_node_title that will call the mocked
-    // node_load() function, and assert that this returns the expected result.
+    /**
+     * Retrieve the results of get_node_title that will call the mocked
+     * node_load() function, and assert that this returns the expected result.
+     */
     $this->assertEquals('Expected title', get_node_title());
   }
 }
