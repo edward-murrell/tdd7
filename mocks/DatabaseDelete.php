e94f5aed4fa0998dafb260a8d209f42bd98d19e4
Adam Bramley <adam@catalyst.net.nz>
2015-02-17 12:07:45 +1100

    Implements execute()

--- mocks/DatabaseDelete.php
+++ mocks/DatabaseDelete.php
@@ -9,18 +9,40 @@ require_once 'DatabaseMockQuery.php';
  */
 class MockDeleteQuery extends MockQuery {
 
-  /**
-   * The table on which to delete from.
-   *
-   * @var string
-   */
   private $tablename;
-
   private $database;
 
+  /**
+   * The condition object for this query.
+   *
+   * Condition handling is handled via composition.
+   *
+   * @var DatabaseCondition
+   */
+  protected $condition;
 
   function __construct(DatabaseConnection_unittest $db, $tablename) {
     $this->database = $db;
     $this->tablename = $tablename;
+  }
+
+  /**
+   * Executes the DELETE query.
+   *
+   * @return
+   *   The number of rows affected by the delete.
+   */
+  public function execute() {
+    $results = array();
+    $tabledata = &$this->database->getTestData($this->tablename);
+    $rows_deleted = 0;
+    foreach ($tabledata as $row_id => &$row) {
+      if ($this->confirmMatch($row)) {
+        unset($tabledata[$row_id]);
+        $rows_deleted++;
+      }
+    }
+
+    return $rows_deleted;
   }
 }
